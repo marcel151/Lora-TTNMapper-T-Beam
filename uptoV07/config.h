7e80771258c81dda0f7c3a145093257539e53aa9
marcel151 <65665570+marcel151@users.noreply.github.com>
2020-06-28 01:36:36 +0200

    uncommented second line + added info
    I uncommented the second line to let the info in the first line make sense, I also added detailed info about lsb and msb, as beginners can miss them fast.

--- uptoV07/config.h
+++ uptoV07/config.h
@@ -1,5 +1,5 @@
 // Comment the next line to use ABP authentication on TTN. Leave it as it is to use recommended OTAA
-//#define OTAA
+#define OTAA
 
 #ifndef LORA_TTNMAPPER_TBEAM_CONFIG_INCLUDED
 #define LORA_TTNMAPPER_TBEAM_CONFIG_INCLUDED
@@ -12,8 +12,17 @@ static u1_t PROGMEM APPSKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x
 static const u4_t DEVADDR = 0x00000000 ; // LoRaWAN end-device address (DevAddr)
 #else
 // Settings from OTAA device
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0x00, 0x00,
+// 0x00.
 static const u1_t PROGMEM DEVEUI[8]={ 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 } ; // Device EUI, hex, lsb
+// This should also be in little endian format, see above.
 static const u1_t PROGMEM APPEUI[8]={ 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 }; // Application EUI, hex, lsb
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
 static const u1_t PROGMEM APPKEY[16] = { 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 }; // App Key, hex, msb
 
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
